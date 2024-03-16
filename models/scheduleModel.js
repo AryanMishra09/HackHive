846477678db7521265583c689539248b619d4d60
Aryan Mishra <mishraaryan9204@gmail.com>
2024-03-16 16:00:58 +0530

    Schedule Model

---
+++ models/scheduleModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { slotTimings } = require("../utils/constants.js");
+const moment = require("moment-timezone");
+
+const ScheduleSchema = new mongoose.Schema({
+  MentorId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Mentor",
+  },
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  slots:[
+    { 
+    type: String, 
+    enum: slotTimings, 
+    }
+  ],
+  createdAt: {
+    type: String,
+    default: () => moment().tz("Asia/Kolkata").format(),
+  }
+});
+
+const ScheduleModel = mongoose.model("Schedule", ScheduleSchema);
+module.exports = ScheduleModel;
